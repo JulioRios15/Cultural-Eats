7931191a0d2e2aae46a564d22e52c272582023af
JulioRios15 <89167159+JulioRios15@users.noreply.github.com>
2022-01-27 16:31:18 -0400

    update config vars

--- server/src/config/vars.js
+++ server/src/config/vars.js
@@ -3,5 +3,9 @@ dotenv.config();
 
 module.exports = {
     port: process.env.PORT || 3001,
-    dbUri: process.env.MONGODB_URI || "mongodb://localhost/cultural-eats-db"
+    dbUri: process.env.MONGODB_URI || "mongodb://localhost/cultural-eats-db",
+    tokenPrivateKey: process.env.TOKEN_PRIVATE_KEY || "private-key",
+    tokenPublicKey: process.env.TOKEN_PUBLIC_KEY || "public-key",
+    tokenTtl: "2h",
+    saltWorkFactor: process.env.SALT_WORK_FACTOR || 10,
 }

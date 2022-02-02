a4851606abcb14e70fbffdf7ed4858635ce0c326
JulioRios15 <89167159+JulioRios15@users.noreply.github.com>
2022-02-01 21:04:49 -0400

    apollo query & mmutation
    - implement apollo server queries & mutations

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,9 +1,76 @@
-import gql from 'graphql-tag';
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      meals {
+        idMeal
+        strMeal
+        strArea
+        strCategory
+        strInstructions
+        strTags
+        strDrinkAlternate
+        strYoutube
+        strSource
+        strMealThumb
+		    instructions
+        mealData {
+          ingredient
+          measure
+        }
+      }
+    }
+  }
+`;
 
 export const QUERY_AREA = gql`
-{
+  {
     area {
       name
+    }
+  }
+`;
+
+export const QUERY_CATEGORY = gql`
+  {
+    category {
+      name
+    }
+  }
+`;
+
+export const QUERY_MEAL = gql`
+  query getMeal($idMeal: String!) {
+    meal(idMeal: $idMeal) {
+      idMeal
+      strMeal
+      strArea
+      strCategory
+      strInstructions
+      strDrinkAlternate
+      strYoutube
+      strSource
+      strTags
+	    strMealThumb
+	    instructions
+      mealData {
+        ingredient
+        measure
+      }
+    }
+  }
+`;
+
+export const QUERY_MEAL_SHORT = gql`
+  query getMealShort($a: String, $c: String, $i: String) {
+    mealShort(a: $a, c: $c, i: $i) {
+      name
+      image
+      idMeal
     }
   }
 `;

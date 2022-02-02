d6e7d7001b99479fe5c2c429ef70e762e609d76b
JulioRios15 <89167159+JulioRios15@users.noreply.github.com>
2022-02-02 15:57:41 -0400

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,5 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import Homepage from "../../pages/HomePage";
-import Recipes from "../../pages/Recipes";
-import SavedRecipes from "../../pages/SavedRecipes";
-import SignIn from "../../pages/SignIn";
-import SignUp from "../../pages/SignUp";
-import Filter from "../../pages/Filter"
 import { NavLink } from "react-router-dom";
 
 export default function Navibar() {
@@ -21,20 +15,7 @@ export default function Navibar() {
               <NavLink activeClassName="active" to="/Filter">Filter</NavLink>
               <NavLink activeClassName="active" to="/Sign-in">Sign In</NavLink>
               <NavLink activeClassName="active" to="/Sign-up">Sign Up</NavLink>
-            </Nav>
-          <Routes>
-              <Route path="/" element={Homepage()}/>
-              <Route path="/Recipes" element={Recipes()}/>
-              <Route path="/Saved-Recipes" element={SavedRecipes()}/>
-              <Route path="/Filter" element={Filter()}/>
-              <Route path="/Sign-in" element={SignIn()}/>
-              <Route path="/Sign-up" element={SignUp()}/>
-            </Routes>
-        </BrowserRouter>
-              <NavLink activeClassName="active" as={NavLink} to="/Saved-Recipes">Saved Recipes</NavLink>
-              <NavLink activeClassName="active" as={NavLink} to="/Grocery-Cart">Grocery Cart</NavLink>
-              <NavLink activeClassName="active" as={NavLink} to="/AuthPage">AuthPage</NavLink>
-            </Nav>           
+            </Nav>         
       </Container>
     </Navbar>  
   );

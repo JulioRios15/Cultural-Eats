ab96b2b7edbaf6c292b6e4a82a4eea00b1cd90b5
JulioRios15 <89167159+JulioRios15@users.noreply.github.com>
2022-02-03 18:40:27 -0400

    update app
    - remove unused imports
    - add recipe details page

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
+
 //Components
 import Navbar from "./components/Navbar/Navbar";
 import styled from "styled-components";
-import {
-  Form,
-  Button,
-  Alert,
-  Card,
-  Container,
-  Row,
-  Col,
-} from "react-bootstrap";
+
 //Pages
 import SavedRecipes from "./pages/SavedRecipes";
 import SignupForm from "./components/Modal/Sign-up";
 import LoginForm from "./components/Modal/Sign-in";
 import Filter from "./pages/Filter";
 import Landing from "./pages/Landing";
+import RecipeDetails from './pages/RecipeDetails';
 
 export default function App() {
   return (
@@ -35,11 +23,12 @@ export default function App() {
         </StyledNavbarWrapper>
         <StyledContentWrapper>
           <Routes>
-            <Route path="/" element={Landing()} />
-            <Route path="/Saved-Recipes" element={SavedRecipes()} />
-            <Route path="/Recipes" element={Filter()} />
-            <Route path="/Sign-Up" element={SignupForm()} />
-            <Route path="/Sign-In" element={LoginForm()} />
+            <Route path="/" element={<Landing/>} />
+            <Route path="/Saved-Recipes" element={<SavedRecipes/>} />
+            <Route path="/Recipes" element={<Filter/>} />
+            <Route path="Recipes/:idMeal" element={<RecipeDetails/>} />
+            <Route path="/Sign-Up" element={<SignupForm/>} />
+            <Route path="/Sign-In" element={<LoginForm/>} />
           </Routes>
         </StyledContentWrapper>
       </StyledApp>
